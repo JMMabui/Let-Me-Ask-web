1b70846af01e084cf9ed167ea5c5c6aef30c7ddf
JMMabui <justinsalomon2@gmail.com>
2025-07-11 14:37:23 +0200

    feat: integrate Radix UI components and implement audio recording functionality

---
+++ src/http/use-room-questions.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import type { getRoomQuestionsResponse } from './types/get-room-questions-response';
+
+export function useRoomQuestions(roomId: string) {
+  return useQuery({
+    queryKey: ['get-questions', roomId],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3333/room/${roomId}/questions`
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result: getRoomQuestionsResponse = await response.json();
+      if (!result) {
+        throw new Error('No result found');
+      }
+      return result;
+    },
+  });
+}
